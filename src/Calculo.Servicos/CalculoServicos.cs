61633a5104faa73529452781b56f4eb091851ef4
Rodinei Riboli <rodineiriboli@hotmail.com>
2020-10-19 22:05:46 -0300

    .

--- src/Calculo.Servicos/CalculoServicos.cs
+++ src/Calculo.Servicos/CalculoServicos.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using BaseJuros.Dominio;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -9,38 +10,47 @@ namespace Calculo.Servicos
 {
     public class CalculoServicos : ICalculoServicos
     {
+        HttpClient httpClient = new HttpClient();
         public decimal CalculaJuros(decimal valorInicial, double juros, int tempo)
         {
             return new Dominio.Calculo().CalculaJuros(valorInicial, juros, tempo);
         }
 
-        public async Task<List<Dominio.Calculo>> ConsultaApiBaseJuros()
+        public async Task<List<Juros>> ConsultaApiBaseJuros()
         {
-            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage resposta;
+            try
+            {
+                httpClient.BaseAddress = new Uri("http://localhost:5005");
+                httpClient.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
 
-            httpClient.BaseAddress = new Uri("http://localhost:5005");
-            //httpClient.DefaultRequestHeaders.Accept.Add(
-            //    new MediaTypeWithQualityHeaderValue("application/json"));
+                resposta = await httpClient.GetAsync("/taxaJuros");
+            }
+            catch (Exception ex)
+            {
 
-            var resposta = await httpClient.GetAsync("/taxaJuros");
+                throw ex;
+            }
+
             if (resposta.IsSuccessStatusCode)
             {
                 var valorBaseJuro = await resposta.Content.ReadAsStringAsync();
-                return JsonConvert.DeserializeObject<List<Dominio.Calculo>>(valorBaseJuro);
+                return JsonConvert.DeserializeObject<List<Juros>>(valorBaseJuro);
             }
-            return new List<Dominio.Calculo>();
+            return new List<Juros>();
         }
 
         public double ObtemJurosApiBaseJuros()
         {
-            double j = 5;
+            double j = 0;
             var consulta = ConsultaApiBaseJuros();
             consulta.ContinueWith(task =>
             {
-                var juros = task.Result;
+                List<Juros> juros = task.Result;
                 foreach (var item in juros)
                 {
-                    j = item.Juros;
+                    j = item.ValorJuros;
                     break;
                 }
             },
